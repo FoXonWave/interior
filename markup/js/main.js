ea130003e10168d3613bfa4f0177128fd369f987
Vlad Gorokhov <gvlad284@gmail.com>
2020-11-10 23:26:01 +0300

    404 page. Active tabs. Photo slider in detail. Btn style edit

--- markup/js/main.js
+++ markup/js/main.js
@@ -14,10 +14,29 @@ $(document).ready(function() {
         dots: true
     });
 
-    if( $('.products').length > 0) {
-        let items = $('.items').html();
-        $('.refresh').on('click', function() {
-            $(".items").append(items);
+    $('.slider-big').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        dots: false,
+        asNavFor: '.slider-small'
+      });
+      $('.slider-small').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        asNavFor: '.slider-big',
+        centerMode: true,
+        focusOnSelect: true,
+        dots: false,
+        arrows: false
+      });
+
+    if( $('.products').length > 0) {      
+        let items = $('.items.active').html();  
+        $('.refresh').on('click', function() {            
+            $(".items.active").append(items);
             setTimeout(function() {
                 $([document.documentElement, document.body]).animate({
                     scrollTop: $(".refresh").offset().top
@@ -26,6 +45,22 @@ $(document).ready(function() {
             
         });
     }
+
+    $('.featured .tab').on('click', function() {
+        let name = $(this).attr("data-name");
+        $('.featured .tab').removeClass("active");
+        $('.featured .items').removeClass("active");
+        $('.featured .tab[data-name="'+name+'"]').addClass("active");
+        $('.featured .items[data-name="'+name+'"]').addClass("active");
+    });
+
+    $('.products .tab').on('click', function() {
+        let name = $(this).attr("data-name");
+        $('.products .tab').removeClass("active");
+        $('.products .items').removeClass("active");
+        $('.products .tab[data-name="'+name+'"]').addClass("active");
+        $('.products .items[data-name="'+name+'"]').addClass("active");
+    });
     
 });
 
